cb8d610c9d0a7a25f3bd6da163e2442bf0416a9e
Yaroslav Vorozhko <yaroslav@ivinco.com>
2010-06-04 16:27:53 +0300

    fix navigation, excerpts, search

--- SphinxSearch.php
+++ SphinxSearch.php
@@ -35,7 +35,9 @@ class SphinxSearch
         $query = '';
         if (!empty($keywords) && empty($categories)){
             $starCategory = $this->starQuery($keywords);
-            $query = "@(body,title) {$keywords} | @categories {$starCategory}";
+            //$query = "@(body,title) {$keywords} | @categories {$starCategory}";
+            $query = "(@(body,title) {$keywords} | (@categories {$starCategory}))";
+            //echo $query;exit;
         } else {
             $starCategory = $this->starQuery($categories);
             $query = "@(body,title,categories) {$keywords} @categories ".$starCategory;
